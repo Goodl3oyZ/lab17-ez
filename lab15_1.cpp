71892a9a9f61bb98f102027bd19e14d85446a8fe
neokarn <34811101+neokarn@users.noreply.github.com>
2019-03-10 06:51:54 +0700

    Add files via upload

---
+++ lab15_1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+void randData(double *,int,int);
+
+void findRowSum(const double *,double *,int,int);
+
+void showData(double *,int,int);
+
+int main(){
+	srand(time(0));
+	const int N = 7, M = 7;
+	double data[N][M] = {};
+	double result[N] = {};
+	double *dPtr = data[0];
+	randData(dPtr,N,M);
+	showData(dPtr,N,M);
+	
+	cout << "---------------------------------------------\n";
+	
+	findRowSum(dPtr,result,N,M);
+	showData(result,N,1);
+}
+
+//Write definition of randData(), showData() and findRowSum()
---
+++ lab15_2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+void randData(double *,int,int);
+
+void findColSum(const double *,double *,int,int);
+
+void showData(double *,int,int);
+
+int main(){
+	srand(time(0));
+	const int N = 7, M = 7;
+	double data[N][M] = {};
+	double result[M] = {};
+	double *dPtr = data[0];
+	randData(dPtr,N,M);
+	showData(dPtr,N,M);
+	
+	cout << "---------------------------------------------\n";
+	
+	findColSum(dPtr,result,N,M); 
+	showData(result,1,M);
+}
+
+//Write definition of randData(), showData() and findColSum()
