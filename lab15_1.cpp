ece0b796b89a6d49a38a3e216f405e1b2f25c06a
neokarn <34811101+neokarn@users.noreply.github.com>
2021-02-04 20:07:04 +0700

    Update lab15_1.cpp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -4,23 +4,31 @@
 #include<iomanip>
 using namespace std;
 
+void showData(double *,int,int);
+
 void randData(double *,int,int);
 
 void findRowSum(const double *,double *,int,int);
 
-void showData(double *,int,int);
+void findColSum(const double *,double *,int,int);
 
 int main(){
 	srand(time(0));
 	const int N = 6, M = 8;
 	double data[N][M] = {};
-	double result[N] = {};
+	double sum1[N] = {};
+	double sum2[M] = {};
 	double *dPtr = data[0];
 	randData(dPtr,N,M);
 	showData(dPtr,N,M);
 	
 	cout << "---------------------------------------------\n";
 	
-	findRowSum(dPtr,result,N,M);
-	showData(result,N,1);
+	findRowSum(dPtr,sum1,N,M);
+	showData(sum1,N,1);
+	
+	cout << "---------------------------------------------\n";
+	
+	findColSum(dPtr,sum2,N,M); 
+	showData(sum2,1,M);
 }
